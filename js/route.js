70b73da857b062693f0b637d901a046e6c6b1720
zxul767 <willebaldo.gomez@gmail.com>
2015-02-25 00:14:09 -0600

    Add time estimates to routes (Mockup Stage)

--- js/route.js
+++ js/route.js
@@ -1,13 +1,38 @@
-function showResults() {
-	 var spinner = $('.glyphicon-spin');
-	 spinner.toggleClass('glyphicon-spin');
-	 spinner.hide();
-	 
-	 $('#routes-header').toggleClass('hidden');
-	 $('#routes-list').toggleClass('hidden');
-}
-
 $(document).ready(function() {
-	 window.setTimeout(showResults, 700);
-	 $('#destination').focus();
+	 function showResults() {
+		  var spinner = $('.glyphicon-spin');
+		  spinner.toggleClass('glyphicon-spin');
+		  spinner.hide();
+		  
+		  $('#routes-search-progress').hide();
+		  $('#routes-header').removeClass('hidden');
+		  $('#routes-list').removeClass('hidden');
+	 }
+
+	 function configureTimers() {
+		  $('.dial').knob({
+				'max': 60,
+				'readOnly': true,
+				'width': 90,
+				'height': 90,
+				'fgColor': '#009ADE', // '#0072A1'
+				
+				/* This is a workaround for issue: #214 & #223 on jQuery-Knob 
+				 * https://github.com/aterrien/jQuery-Knob/issues/214
+				 * https://github.com/aterrien/jQuery-Knob/issues/223
+				 */
+				'inline': false, 
+				'format': function(v) { return sprintf('%d min', v); },
+				'thickness': 0.25,
+				'inputColor': 'black'
+		  });
+	 }
+
+	 window.setTimeout(showResults, 1500);
+	 configureTimers();
+	 $('#routes-list li:nth-child(1)').addClass('selected');
+	 $('#routes-list li').click(function() {
+		  $('#routes-list li').removeClass('selected');
+		  $(this).addClass('selected');
+	 });
 });
