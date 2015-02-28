54950deda0d305420f58017e9d7f36f4266aab35
zxul767 <willebaldo.gomez@gmail.com>
2015-02-28 15:51:50 -0600

    Add route directions panel and cost estimates (Mockup Stage)
    Also:
    -Experiment with icons to show which is the fastest and cheapest route
    -Add full set of Metro stations' icons

--- js/route.js
+++ js/route.js
@@ -5,17 +5,19 @@ $(document).ready(function() {
 		  spinner.hide();
 		  
 		  $('#routes-search-progress').hide();
+        
 		  $('#routes-header').removeClass('hidden');
 		  $('#routes-list').removeClass('hidden');
+        $('#route-instructions').removeClass('hidden');
 	 }
 
 	 function configureTimers() {
 		  $('.dial').knob({
 				'max': 60,
 				'readOnly': true,
-				'width': 90,
-				'height': 90,
-				'fgColor': '#009ADE', // '#0072A1'
+				'width': 70,
+				'height': 70,
+				'fgColor': '#009ADE',
 				
 				/* This is a workaround for issue: #214 & #223 on jQuery-Knob 
 				 * https://github.com/aterrien/jQuery-Knob/issues/214
@@ -30,9 +32,10 @@ $(document).ready(function() {
 
 	 window.setTimeout(showResults, 1500);
 	 configureTimers();
-	 $('#routes-list li:nth-child(1)').addClass('selected');
+
+	 $('#routes-list li:nth-child(1)').addClass('selected tab');
 	 $('#routes-list li').click(function() {
 		  $('#routes-list li').removeClass('selected');
-		  $(this).addClass('selected');
+		  $(this).addClass('selected tab');
 	 });
 });
